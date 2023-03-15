662d20bbae636940a9309bafc161231d3894eb31
briantoogee <briantogee@gmail.com>
2023-03-15 12:05:31 +0800

    asdf

--- checkout.php
+++ checkout.php
@@ -1,27 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta property=’og:title’ content='Headshot AI'/>
-<meta property=’og:image’ content='https://i.imgur.com/cdgmmQH.jpg'/>
-<meta property=’og:description’ content='Your perfect headshot is just a click away.'/>
-<meta property=’og:url’ content='https://headshotai.studio'/>
-<meta property='og:image:width' content='1200' />
-<meta property='og:image:height' content='627' />
+<meta property="og:title" content="Headshot AI"/>
+<meta property="og:url" content="https://headshotai.studio"/>
+<meta property="og:image" content="https://i.imgur.com/cdgmmQH.jpg" />
+<meta property="og:description" content="Your perfect headshot is just a click away."/>
+<meta property='og:image:width' content="1200" />
+<meta property='og:image:height' content="627" />
 <!-- TYPE BELOW IS PROBABLY: 'website' or 'article' or look on https://ogp.me/#types -->
 <meta property="og:type" content='website'/>
-    <title>Headshot AI Generator for Business - Corporate Professional Photo Studio Quality</title>
-    <link rel="icon" href="images/favicon.png" type="image/png">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-  <script src="https://js.stripe.com/v3/"></script>
-  <meta charset="UTF-8">
+<meta name="description" conten="Leave the perfect first impression with professional AI headshots. Perfect for your LinkedIn or CV. Level up your career now. ">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11116963981"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  <script>(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');</script>
+  gtag('config', 'AW-11116963981');
+</script>
+<!-- Event snippet for Website traffic conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-11116963981/6RbgCJP1gZIYEI3R_bQp'});
+</script>
+
+  <title>Professional AI Headshot Generator - Studio Quality</title>
+  <link rel="icon" href="images/favicon.png" type="image/png">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+<link href="/dist/output.css" rel="stylesheet">
+<meta charset="UTF-8">
+<script src="jquery-3.6.1.min.js"></script>
+<script>(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');</script>
 <script async src='https://r.wdfl.co/rw.js' data-rewardful='50e123'></script>
 <script>
   rewardful('ready', function() {
@@ -31,12 +46,9 @@
       console.log('No referral present.');
     }
   });
-</script>  
+</script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
 </head>
 
 <!--
--- success.php
+++ success.php
@@ -4,18 +4,30 @@
 <html lang="en">
 <head>
 
-<meta property="og:title" content='Headshot AI'/>
-<meta property="og:image" content='https://i.imgur.com/cdgmmQH.jpg'/>
-<meta property="og:description" content='Your perfect headshot is just a click away.'/>
-<meta property="og:url" content='https://headshotai.studio'/>
-<meta property='og:image:width' content='1200' />
-<meta property='og:image:height' content='627' />
+<meta property="og:title" content="Headshot AI"/>
+<meta property="og:url" content="https://headshotai.studio"/>
+<meta property="og:image" content="https://i.imgur.com/cdgmmQH.jpg" />
+<meta property="og:description" content="Your perfect headshot is just a click away."/>
+<meta property='og:image:width' content="1200" />
+<meta property='og:image:height' content="627" />
 <!-- TYPE BELOW IS PROBABLY: 'website' or 'article' or look on https://ogp.me/#types -->
 <meta property="og:type" content='website'/>
-<meta name= “robots” content=noindex, nofollow”>
+<meta name="description" conten="Leave the perfect first impression with professional AI headshots. Perfect for your LinkedIn or CV. Level up your career now. ">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11116963981"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  <title>Headshot AI - Form Submission</title>
-  <link rel="stylesheet" href="style.css">
+  gtag('config', 'AW-11116963981');
+</script>
+<!-- Event snippet for Website traffic conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-11116963981/6RbgCJP1gZIYEI3R_bQp'});
+</script>
+
+  <title>Professional AI Headshot Generator - Studio Quality</title>
   <link rel="icon" href="images/favicon.png" type="image/png">
   <link rel="stylesheet" href="style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
