f72cebb7fa26e201cf906939c2c8481055a83165
briantoogee <briantogee@gmail.com>
2023-03-20 12:42:24 +0800

    success guideline

--- success.php
+++ success.php
@@ -146,7 +146,7 @@
             <div>
               <br>
               <div class="guideline-image">
-                <img src="images/guideline.webp">
+                <img src="images/guideline.jpeg">
               </div>
             <?php
             $Msg = "";
